109da83d2a46bdaa5e54b650e8c18594a7613e72
Arindam-Trivedi <80457227+Arindam-Trivedi@users.noreply.github.com>
2021-03-19 17:24:07 +0530

    Add files via upload

---
+++ Assets/SC_Camera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Camera : MonoBehaviour
+{
+    public GameObject Player;
+    void Update()
+    {
+        transform.position = Player.transform.position;
+    }
+}
